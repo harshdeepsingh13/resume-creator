edc6454f7f5d0a9363561d3f8f55f5e2104593c1
Harshdeep Singh <harshdeep.singh@tothenew.com>
2019-11-30 13:01:11 +0530

    added Core Skills component in the view.

--- static/src/containerComponents/AllDetails/index.js
+++ static/src/containerComponents/AllDetails/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles.scss';
 import BasicInformation from "../../components/BasicInformation";
 import EducationInformation from "../../components/EducationInformation";
+import CoreSkills from '../../components/CoreSkillsInformation'
 
 const AllDetails = props => {
 	return (
 		<div className="allDetails-container">
 			{/*<BasicInformation/>*/}
-			<EducationInformation/>
+			{/*<EducationInformation/>*/}
+			<CoreSkills/>
 		</div>
 	)
 };
