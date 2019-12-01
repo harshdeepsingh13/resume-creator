7803bb3f125014c398e3db49d54fc50e5a46390a
Harshdeep Singh <harshdeep.singh@tothenew.com>
2019-12-01 12:54:05 +0530

    tabs view component

---
+++ static/src/containerComponents/UniversalTabsView/index.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import './styles.scss';
+import PropTypes from 'prop-types';
+import {STYLE_CONSTANTS} from "../../config/config";
+
+const UniversalTabsView = (props) => {
+	const {tabs, styles} = props;
+	const [currentTab, setCurrentTab] = useState(0);
+
+	return (
+		<div
+			className="universalTabs-container"
+			style={{
+				...styles
+			}}
+		>
+			<div className="tabs">
+				<div className="tabsHeader-container">
+					{
+						tabs.map(({tabHeader}, index) => (
+							<div
+								key={index}
+								className="tabHeader Text-Style-2"
+								style={
+									{
+										borderBottom: currentTab === index &&
+											`2px solid ${STYLE_CONSTANTS.PRIMARY_COLORS.PRIMARY_YELLOWISH}`
+									}
+								}
+								onClick={() => setCurrentTab(index)}
+							>
+								{
+									tabHeader
+								}
+							</div>
+						))
+					}
+				</div>
+				<div className="tabsView-container">
+					{
+						tabs[currentTab].componentToRender
+					}
+				</div>
+			</div>
+		</div>
+	)
+};
+
+UniversalTabsView.propTypes = {
+	tabs: PropTypes.arrayOf(PropTypes.shape({
+		tabHeader: PropTypes.string.isRequired,
+		componentToRender: PropTypes.object.isRequired
+	})).isRequired,
+	styles: PropTypes.object
+};
+
+export default UniversalTabsView
