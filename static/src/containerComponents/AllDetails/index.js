867da4db3edb0ad9b8d6f4a271249a5a62a08b5a
Harshdeep Singh <harshdeep.singh@tothenew.com>
2019-12-01 12:43:33 +0530

    implemented tabs view component

--- static/src/containerComponents/AllDetails/index.js
+++ static/src/containerComponents/AllDetails/index.js
@@ -2,14 +2,35 @@ import React from 'react';
 import './styles.scss';
 import BasicInformation from "../../components/BasicInformation";
 import EducationInformation from "../../components/EducationInformation";
-import CoreSkills from '../../components/CoreSkillsInformation'
+import CoreSkillsInformation from '../../components/CoreSkillsInformation'
+import UniversalTabsView from "../UniversalTabsView";
 
 const AllDetails = props => {
 	return (
 		<div className="allDetails-container">
-			{/*<BasicInformation/>*/}
-			{/*<EducationInformation/>*/}
-			<CoreSkills/>
+			<UniversalTabsView
+				tabs={
+					[
+						{
+							tabHeader: 'Basic Info',
+							componentToRender: <BasicInformation/>
+						},
+						{
+							tabHeader: 'Education Details',
+							componentToRender: <EducationInformation/>
+						},
+						{
+							tabHeader: 'Skills',
+							componentToRender: <CoreSkillsInformation/>
+						}
+					]
+				}
+				styles={
+					{
+						width: '50%'
+					}
+				}
+			/>
 		</div>
 	)
 };
