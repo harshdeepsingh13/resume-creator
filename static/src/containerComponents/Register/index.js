aa63a8c5e08e9991a097f47a4c29785d43721607
Harshdeep Singh <harshdeep.singh@tothenew.com>
2019-12-01 12:50:25 +0530

    implemented tabs view component

--- static/src/containerComponents/Register/index.js
+++ static/src/containerComponents/Register/index.js
@@ -1,67 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './styles.scss';
-import {getQueryObject} from "../../services/utils";
-import {STYLE_CONSTANTS} from "../../config/config";
 import Login from "../../components/Login";
+import UniversalTabsView from "../UniversalTabsView";
 import Signup from "../../components/Signup";
 
 const Register = props => {
-	const {
-		signupStatus,
-		loginStatus,
-		signup,
-		login,
-		reinitializeStatus,
-		partnerId,
-		partnerDetails,
-		error
-	} = props;
-	const query = getQueryObject(props.location.search);
-	const [currentTab, setCurrentTab] = useState((
-		query.tab ?
-			(query.tab === 'login' ? 0 : 1) :
-			0
-	));
 	return (
 		<div className="register-container">
-			<div className="register-tabs-container">
-				<div className="register-tabs-header-container">
-					<div
-						className="register-tabs-header Text-Style-2"
-						style={
-							{
-								borderBottom: currentTab === 0 &&
-									`2px solid ${STYLE_CONSTANTS.PRIMARY_COLORS.PRIMARY_YELLOWISH}`
-							}
+			<UniversalTabsView
+				tabs={
+					[
+						{
+							tabHeader: 'Register',
+							componentToRender: <Signup/>
+						},
+						{
+							tabHeader: 'Login',
+							componentToRender: <Login/>
 						}
-						onClick={() => setCurrentTab(0)}
-					>
-						Login
-					</div>
-					<div
-						className="register-tabs-header Text-Style-2"
-						style={
-							{
-								borderBottom: currentTab === 1 &&
-									`2px solid ${STYLE_CONSTANTS.PRIMARY_COLORS.PRIMARY_YELLOWISH}`
-							}
-						}
-						onClick={() => setCurrentTab(1)}
-					>
-						Register
-					</div>
-				</div>
-				<div className="register-tabs">
-					{
-						currentTab === 0 &&
-						<Login/>
-					}
-					{
-						currentTab === 1 &&
-						<Signup/>
-					}
-				</div>
-			</div>
+					]
+				}
+			/>
 		</div>
 	)
 };
