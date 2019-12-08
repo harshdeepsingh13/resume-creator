57cfc66bf9500da183f3218d1c421dde0b0525b7
Harshdeep Singh <harshdeep.singh@tothenew.com>
2019-12-08 11:55:12 +0530

    added delete projectsInformation route.

--- api/v1/User/index.js
+++ api/v1/User/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const {loginController, registerController, getBasicInformationController, updateBasicInformationController, updateEducationInformationController, getEducationInformationController, updateSkillInformationController, getSkillInformationController, updateWorkExperiencesController, getWorkExperiencesController, getProjectInformationController, updatedProjectInformationController} = require('./user.controller');
+const {loginController, registerController, getBasicInformationController, updateBasicInformationController, updateEducationInformationController, getEducationInformationController, updateSkillInformationController, getSkillInformationController, updateWorkExperiencesController, getWorkExperiencesController, getProjectInformationController, updatedProjectInformationController, deleteProjectInformationController} = require('./user.controller');
 const authenticationMiddleware = require('../../../middlewares/authenticationMiddleware');
 
 const app = express.Router();
@@ -28,5 +28,7 @@ app.post('/workExperienceInformation', authenticationMiddleware, updateWorkExper
 app.get('/projectInformation', authenticationMiddleware, getProjectInformationController);
 
 app.post('/projectInformation', authenticationMiddleware, updatedProjectInformationController);
+
+app.delete('/projectInformation', authenticationMiddleware, deleteProjectInformationController);
 
 module.exports = app;
