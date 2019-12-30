884073aa87487190473746a31f6e92e24c77be17
Harshdeep Singh <harshdeep.singh@tothenew.com>
2019-12-30 10:35:57 +0530

    init templates.

---
+++ static/src/containerComponents/Templates/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.scss';
+import PropTypes from 'prop-types';
+import SolidBlueTemplate from "../../components/Templates/SolidBlueTemplate";
+
+const Templates = props => {
+return (
+	<div className="resumeTemplates-container">
+		<SolidBlueTemplate/>
+	</div>
+)
+};
+
+Templates.propTypes={};
+
+export default Templates
