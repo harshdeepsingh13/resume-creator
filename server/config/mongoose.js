943042326bc3c1856a7387151a676b6298a46c99
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-08-01 15:22:11 +0530

    changes to get ready for heroku deployment.

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,6 +3,7 @@ import config from "./config";
 import mongoose from "mongoose";
 
 const {logger, mongodbConnectionURL} = config;
+
 export default () => {
 	mongoose.connect(mongodbConnectionURL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 		.then(r => logger.info("Connection with DB is successful."))
